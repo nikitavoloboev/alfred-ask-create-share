a73fbd736fc9caff86a5c0eeef1ea675de81f651
Nikita Voloboev <nikita.voloboev@icloud.com>
2017-09-13 12:14:53 +0200

    trying to get update working

--- main.go
+++ main.go
@@ -1,116 +1,142 @@
+//
+// Copyright (c) 2016 Dean Jackson <deanishe@deanishe.net>
+//
+// MIT Licence. See http://opensource.org/licenses/MIT
+//
+// Created on 2016-11-05
+//
+
+/*
+This workflow is an example of how to use AwGo's update API.
+
+Its own version (set in info.plist) is 0.1 and it's pointing to the
+GitHub repo "deanishe/alfred-ssh" (a completely different workflow), which
+is several version ahead.
+
+The Script Filter code loads the date of the last update check, and if
+a check is due, it calls this program with the "check" command via
+AwGo's background job API.
+
+When run with "check", the program calls Workflow.CheckForUpdate() to cache
+the available releases.
+
+After that has completed, subsequent runs of the Script Filter will
+show an "Update available!" item (if the query is empty).
+
+Actioning (hitting ↩ or ⌘+1) or completing the item (hitting ⇥)
+auto-completes the query to "workflow:update", which is the keyword for
+one of AwGo's "magic" actions.
+
+At this point, AwGo will take control of execution, and
+download & install the newer version of the workflow (but as it's pointing
+to a different workflow's repo, Alfred will install a different workflow
+rather than actually updating this one).
+*/
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
 	"log"
-	"os"
+	"os/exec"
 
 	"git.deanishe.net/deanishe/awgo"
 	"git.deanishe.net/deanishe/awgo/update"
-	"gopkg.in/alecthomas/kingpin.v2"
+	"github.com/docopt/docopt-go"
 )
 
-// name of the background job that checks for updates
+// Name of the background job that checks for updates
 const updateJobName = "checkForUpdate"
 
+var usage = `alfred-ask-create-share [search|check] [<query>]
+
+Open web submissions from Alfred
+
+Usage:
+	alfred-ask-create-share search [<query>]
+    alfred-ask-create-share check
+    alfred-ask-create-share -h
+
+Options:
+    -h, --help    Show this message and exit.
+`
+
 var (
-	// kingpin and script
-	app *kingpin.Application
-
-	// app commands
-	filterSubmissionsCmd *kingpin.CmdClause
-
-	// script options
-	query string
-
-	// icons
-	// redditIcon    = &aw.Icon{Value: "icons/reddit.png"}
-	// docIcon       = &aw.Icon{Value: "icons/doc.png"}
-	// gitubIcon     = &aw.Icon{Value: "icons/github.png"}
-	// forumsIcon    = &aw.Icon{Value: "icons/forums.png"}
-	// translateIcon = &aw.Icon{Value: "icons/translate.png"}
-	// stackIcon     = &aw.Icon{Value: "icons/stack.png"}
-	// iconAvailable = &aw.Icon{Value: "icons/update-available.png"}
-
-	repo = "nikitavoloboev/alfred-ask-create-share"
-
-	// workflow
-	wf *aw.Workflow
+	iconAvailable = &aw.Icon{Value: "icons/update.png"}
+	repo          = "nikitavoloboev/ask-create-share"
+	wf            *aw.Workflow
 )
 
 func init() {
 	wf = aw.New(update.GitHub(repo))
-
-	app = kingpin.New("ask-create-share", "make web submissions from Alfred")
-	// app.HelpFlag.Short('h')
-	app.Version(wf.Version())
-
-	filterSubmissionsCmd = app.Command("filter", "filters submissions")
-
-	for _, cmd := range []*kingpin.CmdClause{filterSubmissionsCmd} {
-		cmd.Flag("query", "search query").Short('q').StringVar(&query)
-	}
-
-	// list action commands
-	app.DefaultEnvars()
-}
-
-// _actions
-
-// fills Alfred with hash map values and shows keys
-func filterResults(links map[string]string) {
-
-	for key, value := range links {
-		wf.NewItem(key).Valid(true).UID(key).Var("URL", value).Var("ARG", key)
-	}
-
-	wf.Filter(query)
-	wf.SendFeedback()
 }
 
 func run() {
-	var err error
+	// Pass wf.Args() to docopt because our update logic relies on
+	// AwGo's magic actions.
+	args, _ := docopt.Parse(usage, wf.Args(), true, wf.Version(), false, true)
 
-	// load values from file to a hash map
-	f, err := os.Open("ask-create-share.csv")
-	if err != nil {
-		panic(err)
+	// Alternate action: Get available releases from remote
+	if args["check"] != false {
+		wf.TextErrors = true
+		log.Println("Checking for updates...")
+		if err := wf.CheckForUpdate(); err != nil {
+			wf.FatalError(err)
+		}
+		return
 	}
-	defer f.Close()
-
-	r := csv.NewReader(f)
-
-	records, err := r.ReadAll()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// holds user's search arguments and an appropriate search URL
-	links := make(map[string]string)
-
-	for _, record := range records {
-		links[record[0]] = record[1]
+	// ----------------------------------------------------------------
+	// Script Filter
+	var query string
+	if args["<query>"] != nil {
+		query = args["<query>"].(string)
 	}
 
-	// _arg parsing
-	cmd, err := app.Parse(wf.Args())
-	if err != nil {
-		wf.FatalError(err)
+	log.Printf("query=%s", query)
+
+	// Call self with "check" command if an update is due and a
+	// check job isn't already running.
+	if wf.UpdateCheckDue() && !aw.IsRunning(updateJobName) {
+		log.Println("Running update check in background...")
+		cmd := exec.Command("./update", "check")
+		if err := aw.RunInBackground(updateJobName, cmd); err != nil {
+			log.Printf("Error starting update check: %s", err)
+		}
 	}
 
-	switch cmd {
-	case filterSubmissionsCmd.FullCommand():
-		filterResults(links)
-	default:
-		err = fmt.Errorf("unknown command: %s", cmd)
+	if query == "" { // Only show update status if query is empty
+		// Send update status to Alfred
+		if wf.UpdateAvailable() {
+			wf.NewItem("Update available!").
+				Subtitle("↩ to install").
+				Autocomplete("workflow:update").
+				Valid(false).
+				Icon(iconAvailable)
+		}
 	}
 
-	if err != nil {
-		wf.FatalError(err)
+	// Script Filter results
+	for i := 1; i <= 20; i++ {
+		t := fmt.Sprintf("Item #%d", i)
+		wf.NewItem(t).
+			Icon(aw.IconFavourite).
+			Arg(t).
+			Valid(true)
 	}
+
+	// Add an extra item to reset update status for demo purposes
+	wf.NewItem("Reset update status").
+		Valid(false).
+		Autocomplete("workflow:delcache").
+		Icon(aw.IconTrash)
+
+	if query != "" {
+		wf.Filter(query)
+	}
+
+	wf.WarnEmpty("No matching items", "Try a different query?")
+	wf.SendFeedback()
 }
 
 func main() {
-	aw.Run(run)
+	wf.Run(run)
 }
