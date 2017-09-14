1ea8d31b81e2d21cde68721ea950f9c1fe277cf3
Nikita Voloboev <nikita.voloboev@icloud.com>
2017-09-14 02:55:56 +0200

    working auto update

--- main.go
+++ main.go
@@ -1,42 +1,9 @@
-//
-// Copyright (c) 2016 Dean Jackson <deanishe@deanishe.net>
-//
-// MIT Licence. See http://opensource.org/licenses/MIT
-//
-// Created on 2016-11-05
-//
-
-/*
-This workflow is an example of how to use AwGo's update API.
-
-Its own version (set in info.plist) is 0.1 and it's pointing to the
-GitHub repo "deanishe/alfred-ssh" (a completely different workflow), which
-is several version ahead.
-
-The Script Filter code loads the date of the last update check, and if
-a check is due, it calls this program with the "check" command via
-AwGo's background job API.
-
-When run with "check", the program calls Workflow.CheckForUpdate() to cache
-the available releases.
-
-After that has completed, subsequent runs of the Script Filter will
-show an "Update available!" item (if the query is empty).
-
-Actioning (hitting ↩ or ⌘+1) or completing the item (hitting ⇥)
-auto-completes the query to "workflow:update", which is the keyword for
-one of AwGo's "magic" actions.
-
-At this point, AwGo will take control of execution, and
-download & install the newer version of the workflow (but as it's pointing
-to a different workflow's repo, Alfred will install a different workflow
-rather than actually updating this one).
-*/
 package main
 
 import (
-	"fmt"
+	"encoding/csv"
 	"log"
+	"os"
 	"os/exec"
 
 	"git.deanishe.net/deanishe/awgo"
@@ -71,21 +38,23 @@ func init() {
 }
 
 func run() {
+	// log.Println(wf.Args())
+
 	// Pass wf.Args() to docopt because our update logic relies on
 	// AwGo's magic actions.
 	args, _ := docopt.Parse(usage, wf.Args(), true, wf.Version(), false, true)
 
-	// Alternate action: Get available releases from remote
+	// alternate action: get available releases from remote
 	if args["check"] != false {
 		wf.TextErrors = true
-		log.Println("Checking for updates...")
+		log.Println("checking for updates...")
 		if err := wf.CheckForUpdate(); err != nil {
 			wf.FatalError(err)
 		}
 		return
 	}
-	// ----------------------------------------------------------------
-	// Script Filter
+
+	// _script filter
 	var query string
 	if args["<query>"] != nil {
 		query = args["<query>"].(string)
@@ -93,20 +62,20 @@ func run() {
 
 	log.Printf("query=%s", query)
 
-	// Call self with "check" command if an update is due and a
+	// call self with "check" command if an update is due and a
 	// check job isn't already running.
 	if wf.UpdateCheckDue() && !aw.IsRunning(updateJobName) {
-		log.Println("Running update check in background...")
+		log.Println("running update check in background...")
 		cmd := exec.Command("./alfred-ask-create-share", "check")
 		if err := aw.RunInBackground(updateJobName, cmd); err != nil {
-			log.Printf("Error starting update check: %s", err)
+			log.Printf("error starting update check: %s", err)
 		}
 	}
 
 	if query == "" { // Only show update status if query is empty
 		// Send update status to Alfred
 		if wf.UpdateAvailable() {
-			wf.NewItem("Update available!").
+			wf.NewItem("update available!").
 				Subtitle("↩ to install").
 				Autocomplete("workflow:update").
 				Valid(false).
@@ -114,27 +83,62 @@ func run() {
 		}
 	}
 
-	// Script Filter results
-	for i := 1; i <= 20; i++ {
-		t := fmt.Sprintf("Item #%d", i)
-		wf.NewItem(t).
-			Icon(aw.IconFavourite).
-			Arg(t).
-			Valid(true)
+	links := parseCSV()
+
+	for key, value := range links {
+		wf.NewItem(key).Valid(true).UID(key).Var("URL", value).Var("ARG", key)
 	}
 
-	// Add an extra item to reset update status for demo purposes
-	wf.NewItem("Reset update status").
-		Valid(false).
-		Autocomplete("workflow:delcache").
-		Icon(aw.IconTrash)
+	// script filter results
+	// for i := 1; i <= 20; i++ {
+	// 	t := fmt.Sprintf("Item #%d", i)
+	// 	wf.NewItem(t).
+	// 		Icon(aw.IconFavourite).
+	// 		Arg(t).
+	// 		Valid(true)
+	// }
 
 	if query != "" {
 		wf.Filter(query)
 	}
 
-	wf.WarnEmpty("No matching items", "Try a different query?")
+	wf.WarnEmpty("no matching items", "try a different query?")
 	wf.SendFeedback()
+}
+
+func parseCSV() map[string]string {
+	var err error
+
+	// load values from file to a hash map
+	f, err := os.Open("ask-create-share.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// holds user's search arguments and an appropriate search URL
+	links := make(map[string]string)
+
+	for _, record := range records {
+		links[record[0]] = record[1]
+	}
+
+	return links
+
+}
+
+// fills Alfred with hash map values and shows keys
+func filterResults(links map[string]string) {
+
+	// wf.Filter(query)
+	// wf.SendFeedback()
 }
 
 func main() {
