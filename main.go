9eb8e038d87e83e540a4cf1ec6f7bb4f8066a42c
Nikita Voloboev <nikita.voloboev@icloud.com>
2018-06-16 12:10:41 +0200

    refactor

--- main.go
+++ main.go
@@ -1,16 +1,8 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/alecthomas/kingpin"
 	"github.com/deanishe/awgo"
 	"github.com/deanishe/awgo/update"
-)
-
-// Defaults for Kingpin flags
-const (
-	defaultMaxResults = "100"
 )
 
 var (
@@ -22,13 +14,6 @@ var (
 	stackIcon       = &aw.Icon{Value: "icons/stack.png"}
 	docIcon         = &aw.Icon{Value: "icons/doc.png"}
 
-	// Kingpin and script options
-	app *kingpin.Application
-
-	// Application commands
-	searchCmd *kingpin.CmdClause
-	updateCmd *kingpin.CmdClause
-
 	query string
 
 	repo = "nikitavoloboev/alfred-ask-create-share"
@@ -37,51 +22,12 @@ var (
 	wf *aw.Workflow
 )
 
-// Mostly sets up kingpin commands
 func init() {
 	wf = aw.New(update.GitHub(repo), aw.HelpURL(repo+"/issues"))
-
-	app = kingpin.New("ask", "Open web submissions.")
-
-	// Update command
-	updateCmd = app.Command("update", "Check for new workflow version.").Alias("u")
-
-	// Commands using query
-	searchCmd = app.Command("search", "Search web submissions.").Alias("s")
-
-	// Common options
-	for _, cmd := range []*kingpin.CmdClause{
-		searchCmd,
-	} {
-		cmd.Flag("query", "Search query.").Short('q').StringVar(&query)
-	}
 }
 
 func run() {
-	var err error
-
-	cmd, err := app.Parse(wf.Args())
-	if err != nil {
-		wf.FatalError(err)
-	}
-
-	switch cmd {
-	case searchCmd.FullCommand():
-		err = doSearch()
-	case updateCmd.FullCommand():
-		err = doUpdate()
-	default:
-		err = fmt.Errorf("Uknown command: %s", cmd)
-	}
-
-	// Check for update
-	if err == nil && cmd != updateCmd.FullCommand() {
-		err = checkForUpdate()
-	}
-
-	if err != nil {
-		wf.FatalError(err)
-	}
+	searchSubmissions()
 }
 
 func main() {
--- search.go
+++ search.go
@@ -7,39 +7,6 @@ import (
 	"strings"
 )
 
-// doSearch makes a search for web submissions and returns results to Alfred.
-func doSearch() error {
-	showUpdateStatus()
-
-	log.Printf("query=%s", query)
-
-	links := parseCSV()
-
-	for key, value := range links {
-		if strings.Contains(key, "r: ") {
-			wf.NewItem(key).Valid(true).UID(key).Var("URL", value).Var("ARG", key).Icon(redditIcon)
-		} else if strings.Contains(key, "s: ") {
-			wf.NewItem(key).Valid(true).UID(key).Var("URL", value).Var("ARG", key).Icon(stackIcon)
-		} else if strings.Contains(key, "g: ") {
-			wf.NewItem(key).Valid(true).UID(key).Var("URL", value).Var("ARG", key).Icon(githubIcon)
-		} else if strings.Contains(key, "f: ") {
-			wf.NewItem(key).Valid(true).UID(key).Var("URL", value).Var("ARG", key).Icon(forumsIcon)
-		} else if strings.Contains(key, "d: ") {
-			wf.NewItem(key).Valid(true).UID(key).Var("URL", value).Var("ARG", key).Icon(docIcon)
-		} else {
-			wf.NewItem(key).Valid(true).UID(key).Var("URL", value).Var("ARG", key)
-		}
-	}
-
-	if query != "" {
-		wf.Filter(query)
-	}
-
-	wf.WarnEmpty("No matching items", "Try a different query?")
-	wf.SendFeedback()
-	return nil
-}
-
 // parseCSV parses CSV for links and arguments.
 func parseCSV() map[string]string {
 	var err error
@@ -67,4 +34,39 @@ func parseCSV() map[string]string {
 
 	return links
 
+}
+
+// doSearch makes a search for web submissions and returns results to Alfred.
+func searchSubmissions() error {
+	showUpdateStatus()
+
+	log.Printf("query=%s", query)
+
+	links := parseCSV()
+
+	for key, value := range links {
+		if strings.Contains(key, "r: ") {
+			wf.NewItem(key).Valid(true).UID(key).Var("URL", value).Var("ARG", key).Icon(redditIcon)
+		} else if strings.Contains(key, "s: ") {
+			wf.NewItem(key).Valid(true).UID(key).Var("URL", value).Var("ARG", key).Icon(stackIcon)
+		} else if strings.Contains(key, "g: ") {
+			wf.NewItem(key).Valid(true).UID(key).Var("URL", value).Var("ARG", key).Icon(githubIcon)
+		} else if strings.Contains(key, "f: ") {
+			wf.NewItem(key).Valid(true).UID(key).Var("URL", value).Var("ARG", key).Icon(forumsIcon)
+		} else if strings.Contains(key, "d: ") {
+			wf.NewItem(key).Valid(true).UID(key).Var("URL", value).Var("ARG", key).Icon(docIcon)
+		} else {
+			wf.NewItem(key).Valid(true).UID(key).Var("URL", value).Var("ARG", key)
+		}
+	}
+
+	query = os.Args[1]
+
+	if query != "" {
+		wf.Filter(query)
+	}
+
+	wf.WarnEmpty("No matching items", "Try a different query?")
+	wf.SendFeedback()
+	return nil
 }
